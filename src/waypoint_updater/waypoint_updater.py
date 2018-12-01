fddb368e2df0cd95dc0840ebebb369d9dd474d51
Mark Roche <mark.roche13@alumni.imperial.ac.uk>
2018-12-01 15:58:17 +0000

    01/12/2018: Implemented the following functions: - tl_array_test_cb() -> Callback to receive the real traffic loght info from simulator - tl_array_processing_test() -> Function to process the traffic light info from simulator (i.e. get list of Track Map indices) - get_next_tl_test() -> Function to find the next traffic loght ahead of the vehicle on the track - update_waypoint_velocity() -> Function to update the velocity target for the final_waypoints based on the identified closest traffic light * Uses roughly the same method as Project Walkthrough videos (decrease velocity in proportion to the calculated distance to the desired stopping index)
    Notes on current implementation:
    - Currently calibrated to stop 12 indices before the index corresponding to TL
    - Deceleration before TL is implemented as linear scaling factor of 100
    - To run the test based on Simulator's real reported TL info, set self.so_test_TL = True. To work with a classifier, set this to False
    
    Further updates required before testing with Classifier (see line 261)

--- src/waypoint_updater/waypoint_updater.py
+++ src/waypoint_updater/waypoint_updater.py
@@ -2,7 +2,9 @@
 
 import rospy
 from geometry_msgs.msg import PoseStamped
-from styx_msgs.msg import Lane, Waypoint
+# MR 29/11/2018
+# Added TrafficLightArray below to attampt to test TL functionality
+from styx_msgs.msg import Lane, Waypoint, TrafficLightArray
 ## MR 27/11/2018:
 # Int32 message type is used by /traffic_waypoint
 # http://wiki.ros.org/rospy/Overview/Messages
@@ -15,7 +17,7 @@ from std_msgs.msg import Int32 #MR
 from scipy.spatial import cKDTree #MR
 
 import math
-from math import sqrt
+from math import sqrt #MR
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -33,6 +35,14 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+
+# MR Added 29/11/2018
+# For testing TL functionality before classifier created
+# This is taken from TrafficLight.msg
+UNKNOWN=4
+GREEN=2
+YELLOW=1
+RED=0
 
 
 class WaypointUpdater(object):
@@ -53,6 +63,18 @@ class WaypointUpdater(object):
         #rospy.Subscriber('/obstacle_waypoint', , self.obstacle_cb)
         
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
+		
+        # Set to TRUE to test stopping at traffic lights, based on simulator's real reported
+        # status for traffic lights
+        self.do_test_TL = True #MR
+        if (self.do_test_TL): #MR
+		    # List of traffic light structures sent in topic /vehicle/traffic_lights
+            self.tl_array = None #MR
+			# List of Track Map indices corrsponding to the ytraffic lights in self.tl_array
+            self.tl_idx_list = None #MR
+			# Track Map index for the closest traffic light ahead of the ego vehicle
+            self.closest_tl_en_idx = None #MR
+            rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.tl_array_test_cb) #MR
 
         # TODO: Add other member variables you need below
         # MR 27/11/2018:
@@ -81,17 +103,52 @@ class WaypointUpdater(object):
             if self.track_map and self.current_pose: #MR
 			    # If cKDTree has not yet been created, create the cKDTree
                 if not self.track_cKDTree: #MR
-                    self.create_tree() #MR
+                    self.create_tree() #MR			
+					
                 # Calculate the final waypoints - Function is unfinished
                 self.calculate_final_waypoints() #MR
                 # Publish the final waypoints which have been calculated
                 self.final_waypoints_pub.publish(self.final_waypoints) #MR
                 # Print to logfiles. Publishing rate validated in logfile
-                rospy.loginfo('Final Waypoints published by waypoint_updater node') #MR
-                rospy.loginfo('Waypoint_updater node has been alive for %f seconds', self.get_alive_time()) #MR
+                #rospy.loginfo('Final Waypoints published by waypoint_updater node') #MR
+                #rospy.loginfo('Waypoint_updater node has been alive for %f seconds', self.get_alive_time()) #MR
             # Hold until 20ms has passed
             self.publish_rate.sleep() #MR
 
+    # MR 29/11/2018:
+    # Callback to receive the traffic light array vector from the Simulator (purely for testing purposes before TL classifier is built)
+    def tl_array_test_cb(self, msg): #MR
+        self.tl_array = msg.lights #MR
+
+    # MR 29/11/2018:
+    # Test function to map traffic light array to track map waypoints
+    def tl_array_processing_test(self): #MR
+        # Query the cKDTree to find which Track Map index is closest to the traffic light locations
+        # self.tl_idx_list is a list of the indices corresponding to the traffic lights
+        self.tl_idx_list = [self.track_cKDTree.query((light.pose.pose.position.x, light.pose.pose.position.y))[1] 
+		                    for light in self.tl_array] #MR
+        #for idx in self.tl_idx_list: #MR
+        #    rospy.loginfo('Traffic light found at track map index %d', idx) #MR
+	
+    # MR 29/11/2018
+    # From Simulator TL data /vehicle/traffic_lights, find the closest TL ahead of the vehicle
+    def get_next_tl_test(self):
+        # Initialise local parameters
+        track_idx_closest, list_enum_closest, diff_min_pos = -1, -1, 100000000 #MR
+        for list_enum, track_idx in enumerate(self.tl_idx_list): #MR
+            diff = track_idx - self.ego_idx #MR
+            # If distance is positive but lower than previous
+            if (diff > 0 and diff < diff_min_pos): #MR
+                diff_min_pos = diff #MR
+                track_idx_closest = track_idx #MR
+                list_enum_closest = list_enum #MR
+        if track_idx_closest == -1 or list_enum_closest == -1: #MR
+            # If could now find a traffic light ahead, then take the first occuring traffic lis 
+            track_idx_closest = min(self.tl_idx_list) #MR
+            list_enum_closest = self.tl_idx_list.index(idx_closest) #MR
+        # This is a tuple (x, y). x points to the element of closest TL in tl_idx_list. y points is the Track Map index for this TL
+        self.closest_tl_en_idx = (list_enum_closest, track_idx_closest) #MR
+		
     # MR 27/11/2018:
     # Simple function which returns roughly how long the node has been alive.
     # Matches Writing ROS Node - Lesson 5 
@@ -124,7 +181,7 @@ class WaypointUpdater(object):
         # Set the object pose parameter to the value of the ROS message
         # i.e. each time Simulator publishes a new position / orientation for the vehicle, update our stored values
         self.current_pose = msg #MR
-        rospy.loginfo('Current Pose has been updated in waypoint_updater node') #MR
+        #rospy.loginfo('Current Pose has been updated in waypoint_updater node') #MR
         #pass
 
     def waypoints_cb(self, waypoints):
@@ -132,10 +189,11 @@ class WaypointUpdater(object):
         # MR 27/11/2018:
         # Base Waypoints are only sent once (see Lesson 5 notes on 'Waypoint Updater Node (Partial)').
         # Therefore, when this callback function runs store the track map points
+        #rospy.loginfo("OVERWRITTEN TRACK MAP")
         self.track_map = waypoints #MR
         # Number of points in the track map
         self.track_length = len(self.track_map.waypoints) #MR
-        rospy.loginfo('Track Map has been stored in waypoint_updater node') #MR
+        #rospy.loginfo('Track Map has been stored in waypoint_updater node') #MR
         if self.do_print_track_map_2_file: #MR
             self.send_map_2_file() #MR
         # The following is just test code to check that interface with simulator is working:
@@ -146,20 +204,92 @@ class WaypointUpdater(object):
     # MR 27/11/2018:
     # Function to set the final_waypoints 
     def calculate_final_waypoints(self):
+        # Delete the previously sent final_waypoints
+        self.final_waypoints.waypoints = [] #MR
+		
         # Get the closest track point which is ahead of the ego vehicle
         self.get_closest_track_point_ahead() #MR
-        # If it is possible to locate ego vehicle on track map
+
+        # Create a vector for final_waypoints with base speed component
+        final_waypoints_base_speed = [] #MR
+        # This is the list of Track Map indices which correspond to each of the final waypoints
+        final_wp_idx_vect = [] #MR
+        
+        # If it is possible to locate ego vehicle on track map, then create a list of final_waypoints
+		# Here, extract waypoints from Track Map, but do not correct the velocity yet
+		# Method implemented for setting final waypoints borrows heavily from Project Walkthrough videos
         if self.ego_idx: #MR
-            ## Assign from the track map to the final waypoint vector 
-            # Remove the previously sent final_waypoints
-            self.final_waypoints.waypoints = [] #MR
             for idx in range(LOOKAHEAD_WPS): #MR
                 # track_idx should handles wrap-around from end to beginning of the track
                 # Note: This current approach only works for one driving direction
                 track_idx = (idx + self.ego_idx) % self.track_length #MR
+				# List of track waypoints corresponding to final waypoints
+                final_wp_idx_vect.append(track_idx) #MR
                 # Set the final_waypoints, to be published in __init___ function
-                self.final_waypoints.waypoints.append(self.track_map.waypoints[track_idx]) #MR
+                final_waypoints_base_speed.append(self.track_map.waypoints[track_idx]) #MR
+        
+		# This is the function which handles TL adjustments (speed)
+        self.update_waypoint_velocity(final_waypoints_base_speed, final_wp_idx_vect) #MR
+		
+		
+    # MR 01/12/2018:
+	# Update the velocity of the final_waypoints, from the values given in the track map.
+    def update_waypoint_velocity(self, final_waypoints_base_speed, final_wp_idx_vect):
+		# MR 01/12/2018:
+        # Set the velocity profile for the final waypoints
+        # Currently, only run this code if testing without TL classifier implemented
+		# Method implemented here for reducing speed borrows heavily from Project Walkthrough videos
+		
+        # MR 29/11/2018
+        # Only for testing TL functionality prior to developing classifier
+        if self.do_test_TL: #MR
+            if not self.tl_array:
+                # Have not yet received info from Simulator to update the speed. Therefore don't update the speed.
+                self.final_waypoints.waypoints = final_waypoints_base_speed #MR
+                return #MR
+            else:
+                # If testing without classifier, update self.tl_idx_list based on /vehicle/traffic_lights
+                self.tl_array_processing_test() #MR
+                # Get the parameters for the next TL ahead
+                self.get_next_tl_test() #MR
+                (tl_en, tl_idx) = self.closest_tl_en_idx #MR
+                stop_idx = max(0, tl_idx - 12) # MR - Want to stop 12 track points before the TL
+		        # This is the next traffic light ahead of the vehicle
+                tl = self.tl_array[tl_en]
+        else: 
+			# MR 01/12/2018
+			# TO BE COMPLETED !!!
+            # Once classifier is developed, 
+            # Set:
+			# stop_idx
+			# tl
+            self.final_waypoints.waypoints = final_waypoints_base_speed #MR
+            return #MR
 
+        #rospy.loginfo('Car idx: %d, TL Closest: %d', self.ego_idx, tl_idx) #MR
+
+        if tl.state == GREEN: #MR
+			# If traffic light is green, drive at speed specified by Track Map
+            self.final_waypoints.waypoints = final_waypoints_base_speed #MR
+        else:
+			# Method implemented here for reducing speed borrows heavily from Project Walkthrough videos
+            #rospy.loginfo('Ego: %d, TL: %d ', self.ego_idx, stop_idx) #MR
+            #ego_v = self.current_pose.twist.twist.linear.x
+            # Total distance from the ego vehicle to the index which we want to stop at
+            #d_total = self.distance(self.track_map, self.ego_idx, stop_idx)
+            for it, waypoint in enumerate(final_waypoints_base_speed): #MR
+			    W = Waypoint() #MR
+			    W.pose = waypoint.pose #MR
+			    v_init = self.get_waypoint_velocity(waypoint) #MR
+			    wp_idx_on_track = final_wp_idx_vect[it] #MR
+			    d = self.distance(self.track_map.waypoints, wp_idx_on_track, stop_idx) #MR
+			    #v = max(0, min(ego_v, ego_v *(d)/d_total)) #MR
+			    v = v_init * (d / 100) #MR -> SQRT not used. Linear decrease in velocity w/ distance is assumed. Max Decel is not accounted for.
+			    v = 0 if v < 2 else v #MR - as per Project Walkthrough, if velocity low enough, just set target to 0 m/s
+			    W.twist.twist.linear.x = min(v , v_init) #MR - base waypoints hold speed limit - therefore don't exceed this
+			    self.final_waypoints.waypoints.append(W) #MR
+			    #rospy.loginfo('V Target %d: D: %f', it + 1, d) #MR
+			
     def get_closest_track_point_ahead(self):
         # MR 28/11/2018
         use_distance_based = True	
@@ -193,11 +323,16 @@ class WaypointUpdater(object):
 		
         # Set the waypoint index of the ego vehicle
         self.ego_idx = next_idx if is_ahead else closest_idx_in_space #MR
-        rospy.loginfo('Next Waypoint Index: %d', self.ego_idx) #MR
+        #rospy.loginfo('Next Waypoint Index: %d', self.ego_idx) #MR
         
     
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
+        # MR 29/11/2018
+        # rosmsg show std_msgs/Int32 = int32 data. Therefore, use data field to extract value
+        # This will be -1 if not red light detected
+        self.traffic_light_waypoint = msg.data #MR
+        #rospy.loginfo('Traffic Light Waypoint is %d', self.traffic_light_waypoint) #MR
         pass
 
     def obstacle_cb(self, msg):
